b50d8516342eb34d0ac437b4269648e21637b53b
rahulyadav1109 <rydddsss1109@gmail.com>
2023-04-13 18:38:19 +0530

    task10

--- time_pass/main.js
+++ time_pass/main.js
@@ -1,51 +1,13 @@
-const nameInput=document.getElementById('name');
-const emailInput=document.getElementById('email');
+let form=document.getElementById('my-form');
 
-const msg=document.getElementById('msg');
-const myForm=document.getElementById('my-form');
+form.addEventListener('submit', onClick)
 
-myForm.addEventListener('submit', onSubmit);
-
-function onSubmit(e)
-{
+function onClick(e){
     e.preventDefault();
 
-    const name=e.target.name.value;
-    const email=e.target.email.value;
-    
-    let obj={
-        name,email
-    }
+    let name=document.getElementById('name').value;
+    let email=document.getElementById('email').value;
 
-    console.log(obj)
-    localStorage.setItem(obj.email,JSON.stringify(obj))
+    localStorage.setItem(name,email);
 
-    let x=document.getElementById('users');
-    let y=document.createElement('li');
-    y.textContent=obj.name+ " " + obj.email +' ';
-
-    var delbtn=document.createElement('input');
-    delbtn.type='button';
-    delbtn.value='Delete';
-    delbtn.onclick=() => {
-        localStorage.removeItem(obj.email)
-        x.removeChild(y);
-    }
-    const editbtn=document.createElement('input');
-    editbtn.type='button';
-    editbtn.value='Edit';
-
-    editbtn.onclick=() => {
-        localStorage.removeItem(obj.email);
-        x.removeChild(y);
-        document.getElementById('name').value=obj.name;
-        document.getElementById('email').value=obj.email;
-
-    }
-    
-    y.appendChild(delbtn);
-    y.appendChild(editbtn);
-    x.appendChild(y);
-    
 }
-
